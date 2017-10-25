2041f07547fbfbb7f702e1d725268e6df04ec753
root <root@VMdev>
2017-10-25 15:46:29 +1100

    Worked on supan.sh

--- supan.sh
+++ supan.sh
@@ -13,7 +13,7 @@ myUser='root';
 myPassword='password';
 myData='limesurveybdd';
 myTable='limesurvey_users'
-
+usersbdd='sources/usersbdd.txt'
 
 log=/tmp/${0}.log
 
@@ -28,22 +28,21 @@ echo "">${log}
 ##########################
 
 
-#Import usersbdd
+#IMPORT USERSBDD
 
 importUsersbdd(){
 
-    cmd="mysql -u ${myUser} -p${myPassword} ${myData}"
-#    echo ${cmd} >> ${log}
+    MySQL="mysql -u ${myUser} -p${myPassword} ${myData}"
+    
+    cmd="echo \"SELECT CONCAT_WS (' ', uid, users_name) FROM limesurvey_users;\"| ${MySQL} > sources/usersbdd.txt"
+    echo ${cmd} >> ${log}
     eval ${cmd} >> ${log} 2>&1
-   
-#    cmd="SELECT uid, users_name FROM limesurvey_users; > sources/usersbdd.txt"
-#    echo ${cmd} >> ${log}
-#    eval ${cmd} >> ${log} 2>&1
 
+    sed -i '1d' sources/usersbdd.txt
 }
 
 
-#Import supann
+#IMPORT SUPAN
 
 importSupan(){
 
@@ -53,15 +52,17 @@ importSupan(){
     awk '{print $2}' sources/usersbdd.txt > sources/usersname.txt
     
     while read -r LINE; do
-       cmd="ldapsearch -x -H ldaps://ldap.univ-nc.nc -b ou=people,dc=univ-nc,dc=nc -LLL -s sub \"(uid=${LINE})\" supannEntiteAffectation >> outputs/${LINE}.txt)"
+       cmd="ldapsearch -x -H ldaps://ldap.univ-nc.nc -b ou=people,dc=univ-nc,dc=nc -LLL -s sub '(uid=${LINE})' supannEntiteAffectation >> outputs/${LINE}.txt"
        echo ${cmd} >> ${log} 
        eval ${cmd} >> ${log} 2>&1
-    sed 's/[^0-9]*//g' < ${LINE} >${LINE}
+#    sed 's/[^0-9]*//g' < ${LINE} >${LINE}
     done < sources/usersname.txt
 
-#    find . -type f -exec awk -v x=2 'NR==x{exit 1}' {} \; -exec echo rm -f {} \;
+    cmd="cd /outputs/ \; find . -type f -exec awk -v x=2 'NR==x{exit 1}' {} \; -exec echo rm -f {} \;"
+    echo ${cmd} >> ${log} 
+    eval ${cmd} >> ${log} 2>&1
 
-#    sed 's/[^0-9]//g' outputs/*.txt
+    sed 's/[^0-9]//g' outputs/*.txt
     sed 's/^\([0-9][0-9]*\).*$/\1/' outputs/*.txt
 
 }
@@ -76,7 +77,10 @@ importSupan(){
 
 
 importUsersbdd;
-#importSupan;
+importSupan;
+
+echo Fini >> ${log}
 
 cat ${log}
 #rm -f ${log}
+
